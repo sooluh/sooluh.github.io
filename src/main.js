0d8cb854feb5101677f40b29202daf543d1a3ff9
Sool Uh! <suluh.webdevelopers@hotmail.com>
2021-09-07 12:18:34 +0700

    add adsense

--- src/main.js
+++ src/main.js
@@ -3,9 +3,16 @@ import "~/assets/scss/styles.scss";
 import DefaultLayout from "~/layouts/Default.vue";
 
 export default function (Vue, { router, head, isClient }) {
-  // Set default layout as a global component
-  Vue.component("Layout", DefaultLayout);
-
   // Disqus
   Vue.use(VueDisqus, { shortname: "suluh" });
+
+  // Google Adsense
+  head.script.push({
+    type: "text/javascript",
+    src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4435219907558988",
+    asyc: "true"
+  });
+
+  // Set default layout as a global component
+  Vue.component("Layout", DefaultLayout);
 }
